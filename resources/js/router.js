d90500b25d84122a26c80a4d5dc9e1f2f9c89041
MirkOrlando <mirko@orlando-m.it>
2022-06-30 14:12:41 +0200

    create router.js with page Home tbd

---
+++ resources/js/router.js
@@ -0,0 +1,38 @@
+// 0. If using a module system (e.g. via vue-cli), import Vue and VueRouter
+// and then call `Vue.use(VueRouter)`.
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+// 1. Define route components.
+// These can be imported from other files
+import Home from './pages/Home'
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
+
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+const app = new Vue({
+    mode: 'history',
+    router
+}).$mount('#app')
